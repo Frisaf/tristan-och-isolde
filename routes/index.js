3e4fa77b1c33d3a3b3dcd6089c5b395733226c4b
Frisaf <frisaf07@gmail.com>
2025-11-12 11:09:47 +0100

    story routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
-import express from "express"
-import fs from "fs"
+import express from "express";
+import fs from "fs";
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("index.njk", {
         title: "Tristan & Isolde: Remixed",
         message: "A remixed version of Wagner's Tristan & Isolde."
-    })
+    });
 });
 
-export default router
+router.get("/story", (req, res) => {
+    res.render("story.njk", {
+        title: "Prologue",
+        message: "You are Isolde, an intern at Merkeware Inc.; a company that controls the world. You study at Big Land University, but this internship is part of your studies and is required for you to graduate.\nYour mentor during your internship is Tristan, who is very bitter about the fact that he has to take responsibility for you on top of his already stressy work. You think Tristan is a douchebag because of the way he treats you. The help you get is often very vague and you two have barely spoken since you started this internship two weeks ago.\n\nOn top of that, you are rather sad because your dog died the other day. It died because of poisonous water. You don't know how that happened though."
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+
+    res.render("")
+})
+
+export default router;
